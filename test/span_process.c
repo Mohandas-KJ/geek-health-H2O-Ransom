b84ae239761e7fc7cda89ce4e8f14372b1d8bb0f
Mohan Das <mohandaskj.11@gmail.com>
2025-12-30 16:06:38 +0530

    test: Add initial implementation of span_process with hydration error message

---
+++ test/span_process.c
@@ -0,0 +1,40 @@
+#include <windows.h>
+#include <stdio.h>
+
+// Start main function
+int main(int argc, char *argv[]){
+    // Open in Child mode
+    if(argc > 1 && strcmp(argv[1], "child") == 0){
+        MessageBox(
+        NULL,                                                 // Parent Window - That is it doesn't depend on any window
+        "Please have a break\nDrink water to Continue",      // Message to be displayed
+        "Hydration error",                                  // Window Title
+        MB_OK|MB_ICONERROR|MB_SYSTEMMODAL                  // flags
+    );                                                                      
+    return 0;
+    }
+
+    STARTUPINFO si;
+    PROCESS_INFORMATION pi;
+
+    ZeroMemory(&si,sizeof(si));
+    si.cb = sizeof(si);
+    ZeroMemory(&pi,sizeof(pi));
+
+    for(int i = 0; i < 12; i++){
+        CreateProcess(
+            NULL,
+            "span_process.exe child",
+            NULL,
+            NULL,
+            FALSE,
+            CREATE_NEW_CONSOLE,
+            NULL,
+            NULL,
+            &si,
+            &pi
+        );
+    }
+
+    return 0;
+}
