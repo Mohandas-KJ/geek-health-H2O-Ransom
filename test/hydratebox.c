88c70ef6e38e5dfb78de496df6ee3631af328a02
Mohan Das <mohandaskj.11@gmail.com>
2025-12-30 12:40:11 +0530

    Update .gitignore to correctly ignore executable files and add hydration error message box test implementation

--- test/hydratebox.c
+++ test/hydratebox.c
@@ -1,1 +1,12 @@
 // This file is Focused on Learning about the Error message box required for the project
+#include <windows.h>
+
+int main(){
+      MessageBox(
+        NULL,
+        "Please have a break\nDrink water to Continue",
+        "Hydration error",
+        MB_OK|MB_ICONERROR|MB_SYSTEMMODAL
+    );
+    return 0;
+}
