4f725dc1674fef97648775390e868f51007e95b2
Mohan Das <mohandaskj.11@gmail.com>
2025-12-30 19:28:33 +0530

    Refactor main function to include user input for timer offset and display welcome message

--- src/hydrate.c
+++ src/hydrate.c
@@ -5,7 +5,22 @@
 #include "H2Obanner.h"
 
 int main(){
-    long interval;
+    int inp;
+    long int interval;
+    
+    // The Warm Welcome
+    printBanner(); // Print the Banner for a design
+    printf("\nWELCOME TO H2O RANSOM!\n");
 
-    printBanner();
+    // Get User Input
+    printf("\nEnter Timer Offset (in min): ");
+    if(scanf("%d",&inp) != 1){                                  // Used to evaluate the input given by user
+        printf("Invalid Input or Runtime Error Occured!");
+        exit(-1);
+    }
+    
+    interval = inp * 60 * 1000;           // Convert the Entered Minute input to milliseconds to pass it as DWORD to Sleep function
+    printf("%ld",interval);               // Testing Statement (Works Fine!)
+
+    return 0;
 }
