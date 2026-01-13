cebc18591820c9df267d1a5f548ccdab7b9af4d0
Mohan Das <mohandaskj.11@gmail.com>
2026-01-13 12:43:18 +0530

    port: Replace Windows MessageBox with zenity for Linux compatibility in DisplayError function

--- src/WaterError.c
+++ src/WaterError.c
@@ -1,13 +1,11 @@
 // This is the definition if the header file that contains the function
 // Includes
-#include <windows.h>
 #include "WaterError.h"
 
 void DisplayError(void){
-    MessageBox(
-        NULL,
-        "Please have a break\nDrink water to continue",
-        "Hydration Error",
-        MB_OK | MB_ICONERROR | MB_SYSTEMMODAL
+    system(
+        "zenity --error "
+        "--title=\"Hydration Error\" "
+        "--text=\"Please have a break\nDrink water to continue\""
     );
 }
